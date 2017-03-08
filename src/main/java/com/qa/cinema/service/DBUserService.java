01680b343cdf9742bea6fe6177bf7d67569a414f
unknown <skedar@hotmail.co.uk>
2017-03-08 10:52:55 +0000

    QC2-20 created template for DBUserService

---
+++ src/main/java/com/qa/cinema/service/DBUserService.java
@@ -0,0 +1,34 @@
+/**
+ * @author Stefan Matykiewicz
+ * @version 0.1.0
+ */
+
+package com.qa.cinema.service;
+
+public class DBUserService implements UserService {
+
+	@Override
+	public String getUser(Long idUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String createNewUser(String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateUser(Long idUser, String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteUser(Long idUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
