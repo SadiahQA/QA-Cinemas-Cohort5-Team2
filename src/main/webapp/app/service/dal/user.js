d825b6ee5631f0f9d7d556849ed0657af6f3fd17
unknown <skedar@hotmail.co.uk>
2017-03-08 15:44:04 +0000

    QC2-43 Created user.js (user DAL)

---
+++ src/main/webapp/app/service/dal/user.js
@@ -0,0 +1,30 @@
+/*
+ * Author: Stefan Matykiewicz
+ * Version: 0.1.0
+ */
+"use strict";
+
+(function() {
+
+	angular.module("movieApp").service("userDal", [ "dal", UserDal ]);
+
+	function UserDal(dal) {
+
+		this.findIndividualUser = function() {
+			return dal.http.GET("rest/user/json");
+		};
+
+		this.createNewUser = function(user) {
+			return dal.http.POST("rest/user/json", user);
+		};
+
+		this.updateUser = function(id, user) {
+			return dal.http.PUT("rest/user/json/" + id, user);
+		};
+
+		this.deleteUser = function(id) {
+			return dal.http.DELETE("/rest/user/json/" + id);
+		};
+
+	}
+}());
