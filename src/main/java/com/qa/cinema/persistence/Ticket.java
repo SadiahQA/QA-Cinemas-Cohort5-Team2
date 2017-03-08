406b0d8ac45ec6be557b6167d7a9b63418869277
TheLiteralDevilHimself <deniskastrati13@hotmail.com>
2017-03-08 14:10:43 +0000

    QC2-4 Ticket Entity

---
+++ src/main/java/com/qa/cinema/persistence/Ticket.java
@@ -0,0 +1,71 @@
+package com.qa.cinema.persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * 
+ * @author Denis kastrati
+ * @version 0.0.1
+ *
+ */
+@Entity
+@Table(name = "Ticket")
+public class Ticket {
+
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private Long idTicket;
+
+@ManyToOne
+@JoinColumn(name = "Showing_idShowing", nullable = false)
+private Showing showing;
+
+@ManyToOne
+@JoinColumn(name = "User_idUser", nullable= false)
+private User user;
+
+private String ticketType;
+
+
+
+public Ticket(Showing showing, User user, String ticketType) {
+	
+	this.showing = showing;
+	this.user = user;
+	this.ticketType = ticketType;
+}
+
+public Long getIdTicket() {
+	return idTicket;
+}
+
+public Showing getShowing() {
+	return showing;
+}
+public void setShowing(Showing showing) {
+	this.showing = showing;
+}
+public User getUser() {
+	return user;
+}
+public void setUser(User user) {
+	this.user = user;
+}
+public String getTicketType() {
+	return ticketType;
+}
+public void setTicketType(String ticketType) {
+	this.ticketType = ticketType;
+}
+
+
+
+
+}
