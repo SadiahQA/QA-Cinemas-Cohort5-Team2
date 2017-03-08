ae8ba6daad6ab447a26b02f9140a30bcbd52c872
JoshAGordon <joshuagordon8@hotmail.com>
2017-03-08 14:53:19 +0000

    Added Navbar to Index as basis for all pages and homepage

--- src/main/webapp/app/app-state-manager.js
+++ src/main/webapp/app/app-state-manager.js
@@ -4,17 +4,17 @@
 
     angular.module('movieApp').config(function ($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise("/getmovie");
+        $urlRouterProvider.otherwise("/home");
 
-        $stateProvider.state("dashboard", {
-            url: "/dashboard",
-            templateUrl: "app/feature/dashboard/dashboard-partial.html"
-        }).state("addmovie", {
-            url: "/addmovie",
-            templateUrl: "app/feature/movie/add/add-movie-partial.html"
-        }).state("getmovie", {
-            url: "/getmovie",
-            templateUrl: "app/feature/movie/get/get-movie-partial.html"
+        $stateProvider.state("seecurrentmovies", {
+            url: "/seecurrentmovies",
+            templateUrl: "app/feature/movie/get/get-current/get-current-movies.html"
+        }).state("seefuturemovies",{
+            url:"/seefuturemovies",
+            templateUrl: "app/feature/movie/get/get-future/get-future-movies.html"
+        }).state("homepage", {
+            url: "/home",
+            templateUrl: "app/feature/home/home.html"
         })
     });
 }());
