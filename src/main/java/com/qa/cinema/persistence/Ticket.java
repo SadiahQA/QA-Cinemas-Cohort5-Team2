28529dfcba08a44dabf38e783c05fec69c2e07e8
TheLiteralDevilHimself <deniskastrati13@hotmail.com>
2017-03-08 14:43:00 +0000

    QC2-4 updated entity

--- src/main/java/com/qa/cinema/persistence/Ticket.java
+++ src/main/java/com/qa/cinema/persistence/Ticket.java
@@ -12,60 +12,65 @@ import javax.persistence.Table;
 /**
  * 
  * @author Denis kastrati
- * @version 0.0.1
+ * @version 0.0.2
  *
  */
+
 @Entity
 @Table(name = "Ticket")
 public class Ticket {
 
-@Id
-@GeneratedValue(strategy = GenerationType.AUTO)
-private Long idTicket;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long idTicket;
 
-@ManyToOne
-@JoinColumn(name = "Showing_idShowing", nullable = false)
-private Showing showing;
+	@ManyToOne
+	@JoinColumn(name = "Showing_idShowing", nullable = false)
+	private Showing showing;
 
-@ManyToOne
-@JoinColumn(name = "User_idUser", nullable= false)
-private User user;
+	@ManyToOne
+	@JoinColumn(name = "User_idUser", nullable = false)
+	private User user;
 
-private String ticketType;
+	private String ticketType;
 
+	public Ticket() {
 
+	}
 
-public Ticket(Showing showing, User user, String ticketType) {
-	
-	this.showing = showing;
-	this.user = user;
-	this.ticketType = ticketType;
-}
+	public Ticket(Showing showing, User user, String ticketType) {
 
-public Long getIdTicket() {
-	return idTicket;
-}
+		this.showing = showing;
+		this.user = user;
+		this.ticketType = ticketType;
+	}
 
-public Showing getShowing() {
-	return showing;
-}
-public void setShowing(Showing showing) {
-	this.showing = showing;
-}
-public User getUser() {
-	return user;
-}
-public void setUser(User user) {
-	this.user = user;
-}
-public String getTicketType() {
-	return ticketType;
-}
-public void setTicketType(String ticketType) {
-	this.ticketType = ticketType;
-}
+	public Long getIdTicket() {
+		return idTicket;
+	}
 
+	public Showing getShowing() {
+		return showing;
+	}
 
+	public void setShowing(Showing showing) {
+		this.showing = showing;
+	}
 
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getTicketType() {
+		return ticketType;
+	}
+
+	public void setTicketType(String ticketType) {
+		this.ticketType = ticketType;
+	}
 
 }
