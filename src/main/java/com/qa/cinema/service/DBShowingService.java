ec35bcf335ed12a7e3313a5af924658490f09701
SadiahQA <sadiah.ahmed@hotmail.co.uk>
2017-03-08 17:13:17 +0000

    QC2-15 Created Showing service, Map showing service, Showing service

---
+++ src/main/java/com/qa/cinema/service/DBShowingService.java
@@ -0,0 +1,40 @@
+package com.qa.cinema.service;
+
+import java.util.Collection;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.qa.cinema.persistence.Cinema;
+import com.qa.cinema.persistence.Movie;
+import com.qa.cinema.persistence.Showing;
+import com.qa.cinema.util.JSONUtil;
+
+/**		
+ * @author Sadiah Ahmed		
+ * @version 0.0.1		
+ * @since 08/03/2017		
+ *		
+ */
+
+@Stateless
+@Default
+public class DBShowingService implements ShowingService {
+	
+	@PersistenceContext(unitName = "primary")
+	private EntityManager em;
+	
+	@Inject
+	private JSONUtil util;
+	
+	@Override
+	public String getAllShowingsForMovie(Movie movie) {
+		Query query = em.createQuery("Select s FROM Showing s where s.movie =" + movie);
+		Collection<Showing> showing = (Collection<Showing>) query.getResultList();
+		return util.getJSONForObject(showing); 
+	}
+}
---
+++ src/main/java/com/qa/cinema/service/ShowingService.java
@@ -0,0 +1,16 @@
+package com.qa.cinema.service;
+
+import com.qa.cinema.persistence.Cinema;
+import com.qa.cinema.persistence.Movie;
+
+/**		
+ * @author Sadiah Ahmed		
+ * @version 0.0.1		
+ * @since 08/03/2017		
+ *		
+ */
+
+public interface ShowingService {
+	
+	String getAllShowingsForMovie(Movie movie);
+}
