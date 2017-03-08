59ea6bc6f683ab7a48d0ef89e816a34dd38ded28
RichaJP93 <richardsonjp93@gmail.com>
2017-03-08 10:41:45 +0000

    QC2-14 created Screen entity

---
+++ src/main/java/com/qa/cinema/persistence/Screen.java
@@ -0,0 +1,80 @@
+
+package com.qa.cinema.persistence;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * 
+ * 
+ * @author Joseph Richardson
+ * @version 0.0.1
+ */
+
+@Entity
+public class Screen {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long idScreen;
+	private Long screenNum;
+	private int noOfSeats;
+	private String screenType;
+	private boolean isDirectorsBox;
+	private List<Showing> showings;
+	
+	public Screen(){
+		
+	}
+
+	public Screen(Long idScreen, Long screenNum, int noOfSeats, String screenType, boolean isDirectorsBox) {
+		this.idScreen = idScreen;
+		this.screenNum = screenNum;
+		this.noOfSeats = noOfSeats;
+		this.screenType = screenType;
+		this.isDirectorsBox = isDirectorsBox;
+		showings = new ArrayList<Showing>();
+	}
+	
+	
+	public Long getId() {
+		return idScreen;
+	}
+
+
+	public Long getScreenNum() {
+		return screenNum;
+	}
+	
+	public void setScreenNum(Long screenNum) {
+		this.screenNum = screenNum;
+	}
+	
+	public int getNoOfSeats() {
+		return noOfSeats;
+	}
+	
+	public void setNoOfSeats(int numOfSeats) {
+		this.noOfSeats = numOfSeats;
+	}
+	public String getScreenType() {
+		return screenType;
+	}
+	public void setScreenType(String screenType) {
+		this.screenType = screenType;
+	}
+	public boolean isDirectorsBox() {
+		return isDirectorsBox;
+	}
+	public void setDirectorsBox(boolean isDirectorsBox) {
+		this.isDirectorsBox = isDirectorsBox;
+	}
+
+	public List<Showing> getShowings() {
+		return showings;
+	}
+
+}
