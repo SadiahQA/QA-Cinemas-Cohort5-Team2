11a288a307e739569e0f7ef961e7f3ce4bdafa09
AliFenn <ali.fenn@hotmail.co.uk>
2017-03-08 14:24:22 +0000

    sorted imports

--- src/main/java/com/qa/cinema/persistence/Cinema.java
+++ src/main/java/com/qa/cinema/persistence/Cinema.java
@@ -1,8 +1,7 @@
 package com.qa.cinema.persistence;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.validation.executable.ValidateOnExecution;
+
 
 /*
  * Author: Alistair Fenn
