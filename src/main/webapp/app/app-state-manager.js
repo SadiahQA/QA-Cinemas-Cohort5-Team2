216e9fcc5be5e324593ef35b7945a9adc77ec919
AlexMercer <alex.mercer2229@googlemail.com>
2017-03-09 14:41:58 +0000

    updated home page and app-state manager to function as currently intended

--- src/main/webapp/app/app-state-manager.js
+++ src/main/webapp/app/app-state-manager.js
@@ -6,19 +6,19 @@
 
         $urlRouterProvider.otherwise("/home");
 
-        $stateProvider.state("seecurrentmovies", {
-            url: "/seecurrentmovies",
-            templateUrl: "app/feature/movie/get/get-current/get-current-movies.html"
-        }).state("seefuturemovies",{
-            url:"/seefuturemovies",
-            templateUrl: "app/feature/movie/get/get-future/get-future-movies.html"
-        }).state("seeonemovie",{
-            url:"/seeonemovie",
-            params:['movieId'],
-            templateUrl: "app/feature/move/get/get-one/get-one-movie.html"
-        }).state("homepage", {
+        $stateProvider.state("homepage", {
             url: "/home",
             templateUrl: "app/feature/home/home.html"
+        }).state("seecurrentmovies", {
+            url: "/seecurrentmovies",
+            templateUrl: "app/feature/movie/get/get-current-movies/get-current-movies.html"
+        }).state("seefuturemovies",{
+            url:"/seefuturemovies",
+            templateUrl: "app/feature/movie/get/get-future-movies/get-future-movies.html"
+        }).state("seeonemovie",{
+            url:"/seeonemovie",
+            //params:['movieId'],
+            templateUrl: "app/feature/move/get/get-one/get-one-movie.html"
         })
     });
 }());
