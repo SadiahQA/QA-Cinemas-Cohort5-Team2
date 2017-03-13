31ba99a01828ba4f0b74bdf0eb60997318476705
SadiahQA <sadiah.ahmed@hotmail.co.uk>
2017-03-13 15:31:15 +0000

    QC2-38 Created dal file for Showing entity

---
+++ src/main/webapp/app/service/dal/showing.js
@@ -0,0 +1,18 @@
+/**		
+ * @author Sadiah Ahmed		
+ * @version 0.0.1		
+ * @since 13/03/2017		
+ *		
+ */
+
+"use strict";
+
+(function(){
+	angular.module("movieApp").service("showingDal",["dal",ShowingDal]);
+	
+	function ShowingDal(dal) {
+		this.getShowingsForMovie = function(idMovie,idCinema) {
+			return dal.http.GET("rest/showing/json/" + idMovie + "/" +idCinema);
+		}
+	}
+}());
