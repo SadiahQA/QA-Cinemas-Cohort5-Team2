7a767e84e220a6ff485297b8d67c9df0f1f1c263
Undeadtaz <taryiandrews@gmail.com>
2017-03-14 16:50:18 +0000

    QC2-48 preliminary ticket form made

--- src/main/webapp/app/feature/ticket/add-ticket/add-ticket-controller.js
+++ src/main/webapp/app/feature/ticket/add-ticket/add-ticket-controller.js
@@ -1,6 +1,6 @@
 (function() {
 
-    var AddTicketController =  function( ticketDal) {
+    var AddTicketController =  function( ticketDal, showingDal, movieDal) {
         var vm = this;
 
         vm.addTicket = function(ticketToAdd) {
@@ -16,8 +16,30 @@
                 vm.errorMessage = error;
             });
         }
+        vm.getShowings = function (theMovieId) {
+
+            vm.showings = [];
+
+            showingDal.getShowingsForMovie(theMovieId, 1).then(function (results) {
+                vm.showings[theMovieId] = results;
+            }, function (error) {
+                vm.error = true;
+                vm.errorMessage = error;
+            });
+        }
+        vm.getMovies = function () {
+
+            vm.movies = [];
+
+            movieDal.getCurrentMovies().then(function (results) {
+                vm.movies = results;
+            }, function (error) {
+                vm.error = true;
+                vm.errorMessage = error;
+            });
+        }
 
     };
 
-    angular.module('movieApp').controller('addTicketController', [ 'ticketDal',AddTicketController]);
+    angular.module('movieApp').controller('addTicketController', [ 'ticketDal','showingDal','movieDal', AddTicketController]);
 }());
