7dac37f2c1779986981f0a59c5cd0579e535f8a8
AliFenn <ali.fenn@hotmail.co.uk>
2017-03-14 12:14:47 +0000

    QC2-98 Mockito test for query generation then string conversion

--- src/test/java/com/qa/cinema/service/DBMovieServiceTest.java
+++ src/test/java/com/qa/cinema/service/DBMovieServiceTest.java
@@ -2,13 +2,48 @@ package com.qa.cinema.service;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.hibernate.mapping.Collection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.qa.cinema.persistence.Movie;
+import com.qa.cinema.util.JSONUtil;
+
+@RunWith(MockitoJUnitRunner.class)
 public class DBMovieServiceTest {
+	
+	@Mock
+	private JSONUtil util;
+	
+	@Mock
+	private EntityManager em;
+	@InjectMocks private DBMovieService movieService;
+	
+	@Mock
+	private Query query;
+	
+	@Mock
+	private Movie movie1;
+	
+	private  ArrayList movies = new ArrayList<Object>();
 
 	@Test
-	public void test() {
+	public void testQueryGenerationToString() {
 		
+		Mockito.when(query.getResultList()).thenReturn((List) movies);
+		movies.add(movie1);
+		Mockito.when(util.getJSONForObject(movies)).thenReturn("Movie String info");
+		assertEquals( "Movie String info",  util.getJSONForObject(query.getResultList()));
 	}
 
 }
