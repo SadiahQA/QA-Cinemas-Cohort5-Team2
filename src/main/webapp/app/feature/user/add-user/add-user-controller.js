481c37be58039dec83761134dff92eb8b9ee7a1f
AliFenn <ali.fenn@hotmail.co.uk>
2017-03-15 14:49:22 +0000

    QC2-86 a unique cookie is generated when a user is created

--- src/main/webapp/app/feature/user/add-user/add-user-controller.js
+++ src/main/webapp/app/feature/user/add-user/add-user-controller.js
@@ -1,8 +1,25 @@
 (function() {
 
+	
+	
     var AddUserController =  function($state, userDal) {
         var vm = this;
+        
+        var hash = function(str){
+            
+            var hash = 0;
+            if (str.length == 0) return hash;
+            for (i = 0; i < str.length; i++) {
+                char = str.charCodeAt(i);
+                hash = ((hash<<5)-hash)+char;
+                hash = hash & hash; // Convert to 32bit integer
+            }
+            return hash;
+        
+    }
 
+        
+     
         vm.addUser = function(userToAdd) {
             console.log("This is the value of user to add " + userToAdd);
             console.log(userToAdd);
@@ -10,15 +27,16 @@
             console.log(userToJson);
             userDal.createNewUser(userToAdd).then(function (results) {
                 vm.userAddMessage  = results;
-                
+             
+                document.cookie = hash(userToAdd.email);
                 $state.go('getuser');
             }, function (error) {
                 vm.error = true;
                 vm.errorMessage = error;
             });
         }
-        
-    };
+    }
+    
 
     angular.module('movieApp').controller('addUserController', ['$state','userDal',AddUserController]);
 }());
