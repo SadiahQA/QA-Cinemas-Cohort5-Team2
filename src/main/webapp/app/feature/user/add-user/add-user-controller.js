430a8a38b16ac5aa37d4b349eceb47a99db0e3e4
AliFenn <ali.fenn@hotmail.co.uk>
2017-03-15 15:35:44 +0000

    QC2-86 hopefully improving code rating

--- src/main/webapp/app/feature/user/add-user/add-user-controller.js
+++ src/main/webapp/app/feature/user/add-user/add-user-controller.js
@@ -8,10 +8,11 @@
         var hash = function(str){
             
             var hash = 0;
-            if (str.length == 0) return hash;
-            for (i = 0; i < str.length; i++) {
-                char = str.charCodeAt(i);
-                hash = ((hash<<5)-hash)+char;
+            if (str.length == 0) 
+            	return hash;
+            for (var i = 0; i < str.length; i++) {
+                var char = str.charCodeAt(i);
+                hash = (hash * 31 + char);
                 hash = hash & hash; // Convert to 32bit integer
             }
             return hash;
