200ff2c504e183f28c278bf17c9a8d6ac6ea2d0b
Undeadtaz <taryiandrews@gmail.com>
2017-03-20 09:50:20 +0000

    commiting to pull from master

--- src/main/java/com/qa/cinema/service/DBUserService.java
+++ src/main/java/com/qa/cinema/service/DBUserService.java
@@ -28,6 +28,7 @@ public class DBUserService implements UserService {
 	private JSONUtil util;
 	
 	
+	
 	@Override
 	public String findUserIdByEmail(String email){
 		Query query = em.createQuery("SELECT u.id FROM User u WHERE u.email= '" + email+"' ");
