2dbc707ce367d9e2ba97ac82af6032da5361c9b6
AlexMercer <alex.mercer2229@googlemail.com>
2017-03-21 10:47:08 +0000

    Added trailers to the view one movie page

--- src/main/webapp/app/feature/movie/get/get-one/get-one-movie-controller.js
+++ src/main/webapp/app/feature/movie/get/get-one/get-one-movie-controller.js
@@ -1,17 +1,22 @@
 (function() {
 
-    var GetOneMovieController =  function(movieDal, $stateParams, bookingFactory, showingDal) {
+    var GetOneMovieController =  function(movieDal, $stateParams, $sce, bookingFactory, showingDal) {
         var vm = this;
 
         function getOneMovie() {
             movieDal.getMovieById($stateParams.idMovie).then(function (results) {
                 vm.movie = results;
+                vm.trustedURL = $sce.trustAsResourceUrl(results.trailerURL);
             }, function (error) {
                 vm.error = true;
                 vm.errorMessage = error;
             });
         }
         getOneMovie();
+
+        vm.getTrailer = function getTrailer(src, $sce){
+        }
+
 
         vm.saveBooking = function (booking){
 
@@ -30,6 +35,6 @@
    }
 
     
-    angular.module('movieApp').controller('getOneMovieController', ['movieDal', '$stateParams','bookingFactory', 'showingDal', GetOneMovieController])
+    angular.module('movieApp').controller('getOneMovieController', ['movieDal', '$stateParams', '$sce', 'bookingFactory', 'showingDal', GetOneMovieController])
 
 }());
